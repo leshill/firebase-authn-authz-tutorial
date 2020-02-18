1806c4e67cfa7b73bbf8e92f2de144b4ac9dcab3
Les Hill <leshill@gmail.com>
2020-02-18 11:05:55 -0800

    Add sign out link to <Navigation>

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Navigation: React.FC = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/home">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/landing">
-          Landing
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+const Navigation: React.FC = () => {
+  const onSignOutClicked = (event: React.MouseEvent) => {
+    alert("Sign out clicked");
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/landing">
+            Landing
+          </Link>
+        </li>
+        <li>
+          <a href="#sign_out" onClick={onSignOutClicked}>
+            Sign Out
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navigation;
