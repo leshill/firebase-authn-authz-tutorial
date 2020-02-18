326cb4734ebaae3f351eed374a36fc63027f03cb
Les Hill <leshill@gmail.com>
2020-02-17 20:49:34 -0800

    Implement <SignUp> component without backend

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing: React.FC = () => (
   <div>
     <h1>Landing</h1>
+    <Link to="/sign-up">
+      Sign Up
+    </Link>
   </div>
 );
 
