6ce4179a16c752e49e31196dd4ba0575d6c06e14
Les Hill <leshill@gmail.com>
2020-02-18 20:25:36 -0800

    Split <Navigation> based on Redux sign in status

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux"
 
+import { RootState } from "rootReducer";
 import { useFirebaseContext } from "FirebaseApp";
 
-const Navigation: React.FC = () => {
+const Authenticated: React.FC = () => {
   const firebase = useFirebaseContext();
 
   const onSignOutClicked = (event: React.MouseEvent) => {
@@ -12,26 +14,62 @@ const Navigation: React.FC = () => {
   };
 
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/home">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/landing">
-            Landing
-          </Link>
-        </li>
-        <li>
-          <a href="#sign_out" onClick={onSignOutClicked}>
-            Sign Out
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul>
+      <li>
+        <Link to="/home">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/landing">
+          Landing
+        </Link>
+      </li>
+      <li>
+        <a href="#sign_out" onClick={onSignOutClicked}>
+          Sign Out
+        </a>
+      </li>
+    </ul>
   );
+}
+
+const Unauthenticated: React.FC = () => (
+  <ul>
+    <li>
+      <Link to="/landing">
+        Landing
+      </Link>
+    </li>
+    <li>
+      <Link to="/sign-in">
+        Sign In
+      </Link>
+    </li>
+    <li>
+      <Link to="/sign-up">
+        Sign Up
+      </Link>
+    </li>
+  </ul>
+);
+
+const Navigation: React.FC = () => {
+  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+
+  if (currentUser) {
+    return (
+      <div>
+        <Authenticated/>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Unauthenticated/>
+      </div>
+    );
+  }
 };
 
 export default Navigation;
