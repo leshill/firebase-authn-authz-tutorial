a1951b7ea32e33ea570bfbea965c643e50f0e8d4
Les Hill <leshill@gmail.com>
2020-02-20 13:38:42 -0800

    Show admin role in <Home>

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { useFirebaseContext } from "FirebaseApp";
 
 const Home: React.FC = () => {
   const firebase = useFirebaseContext();
+
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    const user = firebase.auth.currentUser;
+
+    if (user) {
+      user.getIdTokenResult().then((idToken) => {
+        setAdmin(idToken.claims.admin);
+      });
+    }
+  }, [firebase.auth.currentUser]);
 
   return (
     <div>
@@ -16,6 +28,9 @@ const Home: React.FC = () => {
           firebase.auth.currentUser ?
           firebase.auth.currentUser.email : "Not signed in"
         }
+      </p>
+      <p>
+        User is { admin ? "" : "not"} an admin
       </p>
     </div>
   );
