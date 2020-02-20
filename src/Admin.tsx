ceaa5b3d5eb4e112a8a1bf5a118772c79144746c
Les Hill <leshill@gmail.com>
2020-02-20 15:28:03 -0800

    Show list of users to admins

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useFirebaseContext } from "FirebaseApp";
 
 const Admin: React.FC = () => {
+  const firebase = useFirebaseContext();
+
+  const [state, setState] = useState({
+    error: null as string | null,
+    users: [] as any[]
+  });
+  const {error, users} = state;
+
+  useEffect(() => {
+    firebase.listUsers().then((result: firebase.functions.HttpsCallableResult) => {
+      setState({
+        error: null,
+        users: result.data
+      });
+    }).catch((error: {message: string}) => {
+      setState({
+        error: error.message,
+        users: []
+      });
+    });
+  }, [firebase]);
+
   return (
-    <h1>Admin: Users list</h1>
+    <div>
+      <h1>Admin: Users list</h1>
+      { error && <p>{error}</p> }
+      <ul>
+        {
+          users.map(
+            user => (
+              <li key={user.uid}>
+                <ul>
+                  <li>
+                    Email: {user.email}
+                  </li>
+                  <li>
+                    Admin: {user.customClaims.admin ? "true" : "false"}
+                  </li>
+                </ul>
+              </li>
+            )
+          )
+        }
+      </ul>
+    </div>
   );
 };
 
