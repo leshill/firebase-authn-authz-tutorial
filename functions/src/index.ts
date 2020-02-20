8d0230b12e58de04d857d74812dddc47a1c69745
Les Hill <leshill@gmail.com>
2020-02-20 10:02:22 -0800

    Set admin in `onCreate` auth trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,3 +2,20 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 admin.initializeApp();
+
+function setCustomUserClaims(uid: string, claims: any): Promise<void> {
+  return admin.auth().setCustomUserClaims(uid, claims);
+}
+
+export const onCreate = functions.auth.user()
+  .onCreate((user: admin.auth.UserRecord): Promise<void> => {
+    const defaultEmail = functions.config().admin.default_email;
+
+    if (user.email === defaultEmail &&
+        user.providerData.length === 1 &&
+        user.providerData[0].providerId === "password") {
+      return setCustomUserClaims(user.uid, {admin: true});
+    } else {
+      return Promise.resolve();
+    }
+  });
