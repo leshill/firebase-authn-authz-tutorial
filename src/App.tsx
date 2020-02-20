d8e9be55b74626c3f0f7a4846d0c1e22f9c439f5
Les Hill <leshill@gmail.com>
2020-02-20 14:30:09 -0800

    Basic <Admin> component wired into <Navigation>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import store from "store";
+import Admin from "Admin";
 import Navigation from "Navigation";
 import Home from "Home";
 import Landing from "Landing";
@@ -29,6 +30,7 @@ function App() {
                   <Route exact path="/landing" component={Landing}/>
                   <Route exact path="/sign-in" component={SignIn}/>
                   <Route exact path="/sign-up" component={SignUp}/>
+                  <Route exact path="/admin" component={Admin}/>
                   <Redirect from="/*" to="/landing"/>
                 </Switch>
               </div>
